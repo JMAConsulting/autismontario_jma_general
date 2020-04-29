7e64c6a91c069b0bd21e1b751025de6cb9f69727
Monish Deb <deb.monish@gmail.com>
2020-04-29 15:25:40 +0530

    style and additional fixes

--- js/custom-gmap.js
+++ js/custom-gmap.js
@@ -2,13 +2,18 @@
 
   "use strict";
 
-  $('.field--name-field-geolocation').on('click', function(e) {
+  $('.field--name-field-geolocation, .field--name-organization-name').on('click', function(e) {
     var parts = $(this).data('quickedit-field-id').split('/');
     if (parts[0] == 'civicrm_event') {
       window.open('/event-location/' + parts[1], '_blank');
     }
     if (parts[0] == 'civicrm_contact') {
-      window.open('/contact-map/' + parts[1], '_blank');
+      if (parts[2] == 'organization_name') {
+        window.open('/service-listing/' + parts[1], '_blank');
+      }
+      else {
+        window.open('/contact-map/' + parts[1], '_blank');
+      }
     }
   });
 
