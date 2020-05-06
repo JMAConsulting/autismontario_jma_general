9432131c8be7f97adcbe983ce6e5a5a9161b5500
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-05-06 00:46:53 -0400

    AOSL-57 Update translation for locations on long listing and also hide lables on empty fields and also fix metadata and title on full listing and fix facet title for french

--- js/search-facet.js
+++ js/search-facet.js
@@ -1,5 +1,14 @@
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
   $(document).ready(function() {
     $('.facet-empty').parent().hide();
+
+    if (drupalSettings.path.currentLanguage != 'en') {
+      $('.facet-item__value').each(function() {
+        if ($(this).text() === 'Service Listing') {
+          $(this).text('Répertoire des services');
+        }
+      });
+    }
   });
-})(jQuery, Drupal);
+
+})(jQuery, Drupal, drupalSettings);
