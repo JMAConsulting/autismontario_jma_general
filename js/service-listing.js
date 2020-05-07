d2c0f70cdfa0b46304fedd819cf84ff77c1e7c00
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-05-06 22:21:44 -0400

    AOSL-57 Ensure that when searching on the french version end users are taken to the French version of the service listing and also replace <br>s with empty strings and hide duplicate phone numbers

--- js/service-listing.js
+++ js/service-listing.js
@@ -5,6 +5,15 @@
     if ($('.field-content', $(this)).length && $('.field-content', $(this)).text().trim() == '') {
       $(this).hide();
     }
+    if ($('.field-content', $(this)).length) {
+      $('.field-content', $(this)).html($('.field-content', $(this)).html().replace(/<br>/g, ''));
+    }
+  });
+
+  $.each($('.views-field-address-details .right > a'), function() {
+    if ($(this).text() == $('#primary-phone').text()) { 
+      $('#primary-phone').hide();
+    }
   });
 
 })(jQuery, Drupal);
