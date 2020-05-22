55a0de8e0eb4c712066eb6d4ae6123eadf57091f
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-05-21 21:49:40 -0400

    Add in JS to hide map attachement on search if no mappable results are found

---
+++ js/google-map.js
@@ -0,0 +1,9 @@
+(function ($, Drupal) {
+
+  "use strict";
+
+  if (!$('.geolocation-location').length && $('.geolocation-location').length !== -1 && $('.view-id-search_solr_.view-display-id-attachment_1').length) {
+    $('.view-id-search_solr_.view-display-id-attachment_1').hide();
+  }
+
+})(jQuery, Drupal);
