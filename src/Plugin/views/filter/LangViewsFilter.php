93fdeae390578098f2f19e0c845de8b7bdbc8cc8
Seamus Lee <seamuslee001@gmail.com>
2020-05-27 16:01:29 +1000

    Add in missing file

---
+++ src/Plugin/views/filter/LangViewsFilter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\jma_customizations\Plugin\views\filter;
+
+use Drupal\jma_customizations\Plugin\views\filter\SelectViewsFilter;
+
+/**
+ * Filters by lang of entity.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("lang_views_filter")
+ */
+class LangViewsFilter extends SelectViewsFilter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->valueTitle = t('Language filter');
+    $this->field = 'custom_899';
+  }
+
+  /**
+   * Helper function that generates the options.
+   *
+   * @return array
+   *   An array of states and their ids.
+   */
+  public function generateOptions() {
+    $languages = array_merge(
+      \CRM_Core_OptionGroup::values('language_20180621140924'),
+      \CRM_Core_OptionGroup::values('language_of_event_20181119205706')
+    );
+    return $languages;
+  }
+
+}
