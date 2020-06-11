ee7a24233dedc9ef1398b3d6c939dcd9a1da72cf
Monish Deb <deb.monish@gmail.com>
2020-06-11 18:10:27 +0530

    AOSL-73: style fixes

--- js/advanced_search.js
+++ js/advanced_search.js
@@ -2,5 +2,19 @@
    $(document).ready(function() {
       $('#edit-submit-search-solr-').hide();
       $('#edit-actions').find('#edit-submit-search-solr-').show();
+      var simpleSearchLink = "<a href='#' id='simple-search' style='color:#006ba6;float:right;'>" + Drupal.t('Simple Search') + "</a>";
+      var advanceSearchLink = "<a href='#' id='advance-search' style='color:#006ba6;float:right;display:none;'>" + Drupal.t('Simple Search') + "</a>";
+      $('#block-exposedformsearch-solr-page-1 .form-item-search-api-fulltext').append(simpleSearchLink);
+      $('#block-exposedformsearch-solr-page-1 .form-item-search-api-fulltext').append(advanceSearchLink);
+      $('#simple-search').on('click', function(e) {
+        $('#block-exposedformsearch-solr-page-1 #edit-container-container-1').hide();
+        $('#advance-search').show();
+        $(this).hide();
+      });
+      $('#advance-search').on('click', function(e) {
+        $('#block-exposedformsearch-solr-page-1 #edit-container-container-1').show();
+        $('#simple-search').show();
+        $(this).hide();
+      });
     });
 })(jQuery, Drupal);
